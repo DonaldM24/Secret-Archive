06df8bc08efbff417d2bea647a49c5eb331658f7
DonaldM24 <87953892+DonaldM24@users.noreply.github.com>
2021-08-03 11:59:37 -0400

    Fixed css positions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// Header Transition
 const header = document.querySelector('.header-container')
 window.onscroll = function(){
     var top = window.scrollY;
-    if (top >= 200){
+    if (top >= 25){
         header.classList.add('active')
     }else{
         header.classList.remove('active');
