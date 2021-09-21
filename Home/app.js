bc6fe088d83f550861177fc6ee6d9bc137f3fce1
DonaldM24 <87953892+DonaldM24@users.noreply.github.com>
2021-09-21 16:43:06 -0400

    Additions
    Changed format and added mobile view. Still bugs on hamburger menu.

--- Home/app.js
+++ Home/app.js
@@ -1,12 +1,41 @@
-// Header Transition
+//Header Transition
+function viewportToPixels(value) {
+  var parts = value.match(/([0-9\.]+)(vh|vw)/)
+  var q = Number(parts[1])
+  var side = window[['innerHeight', 'innerWidth'][['vh', 'vw'].indexOf(parts[2])]]
+  return side * (q/100)
+}
 const header = document.querySelector('.header-container')
+const mobile = document.querySelector('.hamburger')
 window.onscroll = function(){
-    var top = window.scrollY;
-    if (top >= 900){
-        header.classList.add('active')
-    }else{
-        header.classList.remove('active');
-    }
+  var top = window.scrollY;
+  if (top >= viewportToPixels('100vh')){
+      header.classList.add('active')
+      mobile.classList.add('active')
+  }else{
+      header.classList.remove('active');
+      mobile.classList.remove('active')
+  }
+}
+
+//Menu Icon
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("open");
+    navMenu.classList.toggle("open");
+}
+
+const navLink = document.querySelectorAll(".navitem");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("open");
+    navMenu.classList.remove("open");
 }
 
 // Image Slider
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// Header Transition
-const header = document.querySelector('.header-container')
-window.onscroll = function(){
+//Header Transition
+function viewportToPixels(value) {
+    var parts = value.match(/([0-9\.]+)(vh|vw)/)
+    var q = Number(parts[1])
+    var side = window[['innerHeight', 'innerWidth'][['vh', 'vw'].indexOf(parts[2])]]
+    return side * (q/100)
+  }
+  const header = document.querySelector('.header-container')
+  const mobile = document.querySelector('.hamburger')
+  window.onscroll = function(){
     var top = window.scrollY;
-    if (top >= 25){
+    if (top >= viewportToPixels('25vh')){
         header.classList.add('active')
+        mobile.classList.add('active')
     }else{
         header.classList.remove('active');
+        mobile.classList.remove('active')
     }
-}
+  }
